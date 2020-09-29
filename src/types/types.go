1cc7e65856f6f938a5fb107f4084361bb578faae
Alex Eustis <akeustis@gmail.com>
2020-09-29 03:02:53 -0700

    lexer

--- src/types/types.go
+++ src/types/types.go
@@ -1,2 +1,28 @@
 package types
 
+// System has methods for adding/recognizing types currently in use.
+type System struct {
+}
+
+// Ur is a top-level type.
+type Ur struct {
+	name string
+}
+
+// NewUr with the given name.
+func NewUr(name string) *Ur {
+	return &Ur{
+		name,
+	}
+}
+
+func (u *Ur) String() string {
+	return u.name
+}
+
+// AddUr to the System.
+func (s *System) AddUr(ur *Ur) {
+
+}
+
+// 
